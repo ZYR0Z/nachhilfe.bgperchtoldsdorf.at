7d8021d5a1d77cb5c30ab1fef05f786744c90faa
ZYR0Z <noah.wallentin@icloud.com>
2025-04-30 12:15:42 +0200

    feat: change text landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   // this should just be the landing page which is neatly designed and prompts the user to either directly find offers (search bar or subject selection) and if pressed it will get the user to the /nachhilfe-suchen page which their requested filters
   // we also want a button to start offering tutoring which will prompt the user to login if not already logged in
-  return <>Hier steht echt viel kacke...</>;
+  return <>Hier ist die Startseite (WIP)</>;
 }

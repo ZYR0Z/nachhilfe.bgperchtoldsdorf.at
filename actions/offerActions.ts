d96f1db07f1968b69a21213527e4076d3d3d2b64
ZYR0Z <noah.wallentin@icloud.com>
2025-05-13 19:37:15 +0200

    feat: get caching to work (dynamic tags) might still be broken, but testing in prods a bit more fun

--- actions/offerActions.ts
+++ actions/offerActions.ts
@@ -3,7 +3,7 @@ import { tutoringOffersTable } from "@/db/schema";
 import { unstable_cache } from 'next/cache'
 import { eq } from "drizzle-orm";
 import { db } from "@/db";
-import { revalidatePath, revalidateTag } from "next/cache";
+import { revalidateTag } from "next/cache";
 import { InferResultType } from "@/lib/utils";
 
 export type TutoringOffer = InferResultType<'tutoringOffersTable', { subject: true, tutor: true }>
@@ -21,77 +21,76 @@ export const getAllOffers = unstable_cache(
   { tags: ['offers'] }
 )
 
-export const getOfferById = unstable_cache(
-  async (id: number) => {
-    return await db.query.tutoringOffersTable.findFirst({
-      where: (tutoringOffersTable, { eq }) => eq(tutoringOffersTable.id, id),
-      with: {
-        tutor: true,
-        subject: true,
-      }
-    });
-  },
-  // TODO: we need to make this dynamic
-  [`offer:byID`],
-  { tags: [`offer:byID`] }
-);
+export async function getOfferById(id: number) {
+  return unstable_cache(
+    () => {
+      return db.query.tutoringOffersTable.findFirst({
+        where: (tutoringOffersTable, { eq }) => eq(tutoringOffersTable.id, id),
+        with: {
+          tutor: true,
+          subject: true,
+        }
+      });
+    },
+    ["offer", id.toString()],
+    { tags: ["offer", `offer:id:${id}`] }
+  )();
+}
 
-export const getOffersByTutorId = unstable_cache(
-  async (tutorId: string) => {
-    return await db.query.tutoringOffersTable.findMany({
-      where: (tutoringOffersTable, { eq }) => eq(tutoringOffersTable.tutor_id, tutorId),
-      with: {
-        tutor: true,
-        subject: true,
-      }
-    })
-  },
-  // TODO: we need to make this dynamic
-  [`offer:byTutorId`],
-  { tags: [`offer:byTutorId`] }
-)
+export async function getOffersByTutorId(tutorId: string) {
+  return unstable_cache(
+    () => {
+      return db.query.tutoringOffersTable.findMany({
+        where: (tutoringOffersTable, { eq }) => eq(tutoringOffersTable.tutor_id, tutorId),
+        with: {
+          tutor: true,
+          subject: true,
+        }
+      })
+    },
+    ["offer", tutorId],
+    { tags: ["offer", `offer:tutor:${tutorId}`, `tutor:${tutorId}`] }
+  )()
+}
 
-export const getOffersBySubjectId = async (subjectId: number) => {
-  return await db.query.tutoringOffersTable.findMany({
-    where: (tutoringOffersTable, { eq }) => eq(tutoringOffersTable.subject_id, subjectId),
-    with: {
-      tutor: true,
-      subject: true,
-    }
-  })
+export async function getOffersBySubjectId(subjectId: number) {
+  return unstable_cache(
+    () => {
+      return db.query.tutoringOffersTable.findMany({
+        where: (tutoringOffersTable, { eq }) => eq(tutoringOffersTable.subject_id, subjectId),
+        with: {
+          tutor: true,
+          subject: true,
+        }
+      })
+    },
+    ["offer", subjectId.toString()],
+    { tags: ["offer", `offer:subject:${subjectId}`, `subject:${subjectId}`] }
+  )
 }
 
 // TODO: maybe add offer by grade?? but how do we do this with multiple grades?
-
-// TODO: do we want to have it return the created offer?
 export const createOffer = async (offer: typeof tutoringOffersTable.$inferInsert) => {
   await db.insert(tutoringOffersTable).values(offer)
   revalidateTag('offers')
-  revalidateTag(`offer:byID`)
-  revalidateTag(`offer:byTutorId`)
-  revalidatePath("/angebote/meine-angebote")
+  revalidateTag(`offer:tutor:${offer.tutor_id}`)
 }
 
 export const editOffer = async (id: number, offer: typeof tutoringOffersTable.$inferInsert) => {
   await db.update(tutoringOffersTable)
     .set(offer)
     .where(eq(tutoringOffersTable.id, id))
-  // TODO: do we even need this?
-  revalidatePath("/angebote/meine-angebote")
   revalidateTag(`offers`)
-  // TODO: we need to make this dynamic
-  revalidateTag(`offer:byID`)
+  revalidateTag(`offer:id:${id}`)
 }
 
 // TODO: cascading delete on subject & tutor deletion?
 export const deleteOffer = async (id: number) => {
-  await db.delete(tutoringOffersTable)
-    .where(eq(tutoringOffersTable.id, id))
-  // TODO: do we even need this?
-  revalidatePath("/angebote/meine-angebote")
+  const offer = await db.delete(tutoringOffersTable)
+    .where(eq(tutoringOffersTable.id, id)).returning()
   revalidateTag(`offers`)
-  // TODO: we need to make this dynamic
-  revalidateTag(`offer:byID`)
+  revalidateTag(`offer:tutor:${offer[0].tutor_id}`)
+  revalidateTag(`offer:id:${id}`)
 }
 
 
